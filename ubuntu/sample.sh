ba6c86bce3ff9d8cfdef32dfbe2cf61442ef87fc
alphace-kadono <alphace-kadono@users.noreply.github.com>
2023-01-30 16:38:19 +0900

    Update sample.sh

--- ubuntu/sample.sh
+++ ubuntu/sample.sh
@@ -1,17 +1,9 @@
-if [[ -z ${deployUser} ]];
-then
+if [[ -z ${deployUser} ]]; then
   echo "deployUser is not set"
-  exit 1;
+  exit 1
 fi
 
-echo "deployUser = ${deployUser}"
-
-
-if [[ -z ${PATH} ]];
-then
-  echo "PATH is not set"
-else
-  echo "PATH is set"
-fi
-
-echo "PATH = ${PATH}"
+# deploy ユーザー
+USER="${deployUser}"
+# userdel -r ${USER}
+useradd -m -s /bin/bash ${USER}
